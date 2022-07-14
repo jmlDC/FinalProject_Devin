53de0de2bb97cccf4661210a8ee9a91ccf0b5d1f
JML_dc <mari.delacruz@obf.ateneo.edu>
2022-07-14 17:05:38 +0800

    Finished copy/pasta the code from Ceniza

--- app/src/main/java/cenizadelacruzenriquez/project/finalproject/BookRecyclerViewScreen.java
+++ app/src/main/java/cenizadelacruzenriquez/project/finalproject/BookRecyclerViewScreen.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_book_recycler_view_screen)
 public class BookRecyclerViewScreen extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_book_recycler_view_screen);
-    }
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_book_recycler_view_screen);
+//    }
 }
--- app/src/main/java/cenizadelacruzenriquez/project/finalproject/EditShelfScreen.java
+++ app/src/main/java/cenizadelacruzenriquez/project/finalproject/EditShelfScreen.java
@@ -1,319 +1,91 @@
 package cenizadelacruzenriquez.project.finalproject;
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.karumi.dexter.Dexter;
-import com.karumi.dexter.MultiplePermissionsReport;
-import com.karumi.dexter.listener.multi.BaseMultiplePermissionsListener;
-import com.squareup.picasso.MemoryPolicy;
-import com.squareup.picasso.NetworkPolicy;
-import com.squareup.picasso.Picasso;
+import androidx.appcompat.app.AppCompatActivity;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
-import io.realm.Case;
 import io.realm.Realm;
-import io.realm.RealmResults;
 
-@EActivity(R.layout.activity_edit_user_screen)
-public class EditUserScreen extends AppCompatActivity {
-
-    /* Registration Function */
-
-    SharedPreferences prefs;
+@EActivity(R.layout.activity_edit_shelf_screen)
+public class EditShelfScreen extends AppCompatActivity {
 
     Realm realm;
+    String ownerUUID;
 
-    @ViewById
-    EditText usernameRegisterField;
+    @ViewById(R.id.shelfNameFieldOnEdit)
+    EditText shelfNameFieldOnEdit;
 
-    @ViewById
-    EditText passwordRegisterField;
+    @ViewById(R.id.shelfDescriptionFieldOnEdit)
+    EditText shelfDescriptionFieldOnEdit;
 
-    @ViewById
-    EditText confirmPasswordRegisterField;
+    @ViewById(R.id.saveShelfButtonOnEdit)
+    Button saveShelfButton;
 
-    @ViewById
-    Button saveRegisterButton;
+    @ViewById(R.id.cancelShelfButtonOnEdit)
+    Button cancelShelfButton;
 
-    @ViewById
-    ImageView imageViewEdit;
-
-    @Click(R.id.saveRegisterButton)
-    public void saveRegister(){
-        realm = Realm.getDefaultInstance();
-        prefs = getSharedPreferences("prefs", MODE_PRIVATE);
-        String uuidEditingValue = prefs.getString("uuidEdit", null);
-
-        // Blank name
-        if(usernameRegisterField.getText().toString().equals("")){
-            Toast toast = Toast.makeText(this, "Name must not be blank", Toast.LENGTH_SHORT);
-            toast.show();
-        }
-
-        // Mismatched passwords
-        else if(!passwordRegisterField.getText().toString().equals(confirmPasswordRegisterField.getText().toString())) {
-            Toast toast = Toast.makeText(this, "Confirm password does not match", Toast.LENGTH_SHORT);
-            toast.show();
-        }
-
-        // Success, check for existing users
-        else{
-            // Check for existing users with same username
-            RealmResults<User> matchUsername = realm.where(User.class)
-                    .equalTo("name", usernameRegisterField.getText().toString(), Case.INSENSITIVE)
-                    .notEqualTo("uuid", uuidEditingValue)
-                    .findAll();
-
-            for (User u : matchUsername)
-            {
-                Log.d("Realm results", u.toString());
-            }
-
-            // If a user matches searched
-            if(matchUsername.isEmpty()){
-                realm.beginTransaction();
-                RealmResults<User> matchUuid = realm.where(User.class)
-                        .equalTo("uuid", uuidEditingValue)
-                        .findAll();
-
-                User editedUser = matchUuid.first();
-
-                editedUser.setName(usernameRegisterField.getText().toString());
-                editedUser.setPassword(passwordRegisterField.getText().toString());
-
-                realm.copyToRealmOrUpdate(editedUser);
-                realm.commitTransaction();
-
-                // Clear shared preferences containing uuid for account being edited
-                prefs = getSharedPreferences("prefs", MODE_PRIVATE);
-                SharedPreferences.Editor edit = prefs.edit();
-                edit.putString("uuidEdit", null);
-                edit.apply();
-
-//                RealmResults<User> allResults = realm.where(User.class)
-//                        .findAll();
-
-                Toast toast = Toast.makeText(this, "User edited.", Toast.LENGTH_SHORT);
-                toast.show();
-
-//                Log.d("All users","-------------------------------------------");
-//                for (User u : allResults)
-//                {
-//                    Log.d("Realm results", u.toString());
-//                }
-
-                openLoginScreen();
-            }
-
-            else{
-                Toast toast = Toast.makeText(this, "User already exists", Toast.LENGTH_SHORT);
-                toast.show();
-            }
-
-        }
-
-    }
-
-    @AfterViews
-    public void init()
-    {
-        realm = Realm.getDefaultInstance();
-
-        prefs = getSharedPreferences("prefs", MODE_PRIVATE);
-        String uuidEditingValue = prefs.getString("uuidEdit", null);
-
-        if(uuidEditingValue == null){
-            // Do nothing
-        }
-        else{
-
-            RealmResults<User> matchAccount = realm.where(User.class)
-                    .equalTo("uuid", uuidEditingValue, Case.INSENSITIVE)
-                    .findAll();
-
-            String usernameValue = matchAccount.first().getName();
-            String passwordValue = matchAccount.first().getPassword();
-
-            usernameRegisterField.setText(usernameValue);
-            passwordRegisterField.setText(passwordValue);
-            confirmPasswordRegisterField.setText(passwordValue);
-
-            File getImageDir = getExternalCacheDir();
-            // this will put the image saved to the file system to the imageview
-            if(matchAccount.first().getPath()!=null) {
-                // just a sample, normally you have a diff image name each time
-                File file = new File(getImageDir, matchAccount.first().getPath());
-
-                Picasso.get()
-                        .load(file)
-                        .networkPolicy(NetworkPolicy.NO_CACHE)
-                        .memoryPolicy(MemoryPolicy.NO_CACHE)
-                        .into(imageViewEdit);
-            }
-            else{
-                // Default string
-            }
-        }
-    }
-
-    public void onDestroy()
-    {
-        super.onDestroy();
-        if (!realm.isClosed())
-        {
-            realm.close();
-        }
-    }
-
-    /* Cancel Button Navigation*/
-    @ViewById
-    Button cancelRegisterButton;
-
-    @Click(R.id.cancelRegisterButton)
-    public void cancelRegister(){
-        openLoginScreen();
-    }
-
-    public void openLoginScreen(){
+    @Click(R.id.cancelShelfButtonOnEdit)
+    public void cancel(){
         finish();
     }
 
-    // ----------------
-    public static int REQUEST_CODE_IMAGE_SCREEN = 0;
+    @Click(R.id.saveShelfButtonOnEdit)
+    public void saveShelf(){
+        String uuid = getIntent().getStringExtra("UUIDToEdit");
+        String name = shelfNameFieldOnEdit.getText().toString();
+        String desc = shelfDescriptionFieldOnEdit.getText().toString();
 
-    @Click(R.id.imageViewEdit)
-    public void clickRegisterPicture() { setEditPicture(); }
+        Shelf shelf = new Shelf();
 
-    public void setEditPicture() {
-        ImageActivity_.intent(this).startForResult(REQUEST_CODE_IMAGE_SCREEN);
-    }
+        shelf.setUuid(uuid);
+        shelf.setShelfName(name);
+        shelf.setShelfDescription(desc);
+        shelf.setOwnerUUID(ownerUUID);
 
-    // SINCE WE USE startForResult(), code will trigger this once the next screen calls finish()
-    public void onActivityResult(int requestCode, int responseCode, Intent data)
-    {
-        super.onActivityResult(requestCode, responseCode, data);
+        long count = 0;
 
-        if (requestCode==REQUEST_CODE_IMAGE_SCREEN)
-        {
-            if (responseCode==ImageActivity.RESULT_CODE_IMAGE_TAKEN)
-            {
-                // receieve the raw JPEG data from ImageActivity
-                // this can be saved to a file or save elsewhere like Realm or online
-                byte[] jpeg = data.getByteArrayExtra("rawJpeg");
+        try {
+            realm.beginTransaction();
+            // Log.i("Reg Screen", "I reached after begin.");
+            realm.copyToRealmOrUpdate(shelf);  // save
+            // Log.i("Reg Screen", "I reached after save.");
+            realm.commitTransaction();
+            // Log.i("Reg Screen", "I reached after commit.");
 
-                try {
+            count = realm.where(Shelf.class).equalTo("ownerUUID", ownerUUID).count();
+            Toast toast = Toast.makeText(this, "Saved: "+count, Toast.LENGTH_LONG);
+            toast.show();
 
-                    prefs = getSharedPreferences("prefs", MODE_PRIVATE);
-                    String uuidEditingValue = prefs.getString("uuidEdit", null);
-
-                    String filename = System.currentTimeMillis()+".jpeg";
-                    File savedImage = saveFile(jpeg, filename);
-
-//                    User newUser = new User();
-//                    newUser.setUuid(randomUUID);
-//                    newUser.setPath(filename);
-
-                    realm.beginTransaction();
-
-                    User c = realm.where(User.class).equalTo("uuid",uuidEditingValue).findFirst();
-                    c.setPath(filename);
-
-                    realm.copyToRealmOrUpdate(c);
-                    realm.commitTransaction();
-
-                    File getImageDir = getExternalCacheDir();
-
-                    if (filename != null) {
-                        // just a sample, normally you have a diff image name each time
-                        File file = new File(getImageDir, filename);
-
-                        Picasso.get()
-                                .load(file)
-                                .networkPolicy(NetworkPolicy.NO_CACHE)
-                                .memoryPolicy(MemoryPolicy.NO_CACHE)
-                                .into(imageViewEdit);
-                    }
-
-                }
-                catch(Exception e)
-                {
-                    e.printStackTrace();
-                }
-
-            }
+        } catch (Exception e) {
+            Toast toast = Toast.makeText(this, "Error saving SHELF", Toast.LENGTH_SHORT);
+            toast.show();
+            e.printStackTrace();
         }
+
+        finish();
     }
-
-    private File saveFile(byte[] jpeg, String name) throws IOException
-    {
-        // this is the root directory for the images
-        File getImageDir = getExternalCacheDir();
-
-        // just a sample, normally you have a diff image name each time
-        File savedImage = new File(getImageDir, name);
-
-        FileOutputStream fos = new FileOutputStream(savedImage);
-        fos.write(jpeg);
-        fos.close();
-        return savedImage;
-    }
-
 
     @AfterViews
-    public void checkPermissions()
-    {
+    public void init(){
+        Realm.init(getApplicationContext());
+        realm = Realm.getDefaultInstance();
 
-        // REQUEST PERMISSIONS for Android 6+
-        // THESE PERMISSIONS SHOULD MATCH THE ONES IN THE MANIFEST
-        Dexter.withContext(this)
-                .withPermissions(
-                        Manifest.permission.READ_EXTERNAL_STORAGE,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                        Manifest.permission.CAMERA
+        String uuid = getIntent().getStringExtra("UUIDToEdit");
+        ownerUUID = realm.where(Shelf.class).equalTo("uuid", uuid).findFirst().getOwnerUUID();
+        String shelfName = realm.where(Shelf.class).equalTo("uuid", uuid).findFirst().getShelfName();
+        String shelfDesc = realm.where(Shelf.class).equalTo("uuid", uuid).findFirst().getShelfDescription();
 
-                )
+        shelfNameFieldOnEdit.setText(shelfName);
+        shelfDescriptionFieldOnEdit.setText(shelfDesc);
 
-                .withListener(new BaseMultiplePermissionsListener()
-                {
-                    public void onPermissionsChecked(MultiplePermissionsReport report)
-                    {
-                        if (report.areAllPermissionsGranted())
-                        {
-                            // all permissions accepted proceed
-                            init();
-                        }
-                        else
-                        {
-                            // notify about permissions
-                            toastRequirePermissions();
-                        }
-                    }
-                })
-                .check();
-
-    }
-
-
-    public void toastRequirePermissions()
-    {
-        Toast.makeText(this, "You must provide permissions for app to run", Toast.LENGTH_LONG).show();
-        finish();
+        // ownerUUID = getIntent().getStringExtra("ownerUUID");
     }
 
 }
